595c97e88b1efde0e8b9015f67facbdb0da21181
we1how <2698470157@qq.com>
2026-01-27 22:23:53 +0800

    Initial Commit

---
+++ TimeScore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimeScore
+//
+//  Created by 林伟豪 on 2026/1/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimeScore/TimeScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeScoreApp.swift
+//  TimeScore
+//
+//  Created by 林伟豪 on 2026/1/27.
+//
+
+import SwiftUI
+
+@main
+struct TimeScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
